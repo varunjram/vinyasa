d63f37c3a84a17669f3e7aedf72f5c8b42879cdd
varunjram <varunram.66@gmail.com>
2023-05-15 08:04:59 +0530

    basic routing and nav Header with nav bar creation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {BsBookmarkHeart, BsFilePerson, BsSearch} from "react-icons/bs";
+import {FaOpencart} from "react-icons/fa";
+
+export default function Navigation() {
+  return (
+    <div className="nav-container">
+      <div className="nav-left">
+        <a href="/">
+          <h2 className="text-3xl">Vinyasa</h2>
+        </a>
+      </div>
+      <div className="nav-center">
+        <BsSearch className="inline" />
+        <input className="input" type="search" name="" id="search" placeholder="Search" />
+      </div>
+      <div className="nav-right">
+        <a href="/wishlist">
+          <BsBookmarkHeart />
+        </a>
+        <a href="/cart">
+          <FaOpencart />
+        </a>
+        <a href="/wishlist">
+          <BsFilePerson />
+        </a>
+        <button className="login-btn btn">Login</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Routing.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import {Routes, Route} from "react-router-dom";
+import Home from "../pages/Home";
+import Cart from "../pages/Cart";
+import WishList from "../pages/WishList";
+import Products from "../pages/Products";
+
+export default function Routing() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<WishList />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<Products />} />
+      </Routes>
+    </>
+  );
+}
